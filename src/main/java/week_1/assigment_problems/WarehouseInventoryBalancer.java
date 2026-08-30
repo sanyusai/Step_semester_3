576305e3764d8debbf4aaa51a791f77cdfea56b5
“sanyusai” <sanyusai7@gmail.com>
2026-08-30 11:05:20 +0530

    Add Week 1 assignment problems

---
+++ src/main/java/week_1/assigment_problems/WarehouseInventoryBalancer.java
@@ -0,0 +1,56 @@
+public class WarehouseInventoryBalancer {
+
+    static void analyzeInventory(int[] sectionA, int[] sectionB) {
+
+        int totalA = 0;
+        int totalB = 0;
+
+        for (int i = 0; i < sectionA.length; i++) {
+            totalA += sectionA[i];
+            totalB += sectionB[i];
+        }
+
+        String status;
+
+        if (totalA == totalB) {
+            status = "Balanced";
+        } else {
+            status = "Not Balanced";
+        }
+
+        int highestQuantity = sectionA[0];
+        String highestSection = "Section A";
+        int highestIndex = 0;
+
+        for (int i = 0; i < sectionA.length; i++) {
+
+            if (sectionA[i] > highestQuantity) {
+                highestQuantity = sectionA[i];
+                highestSection = "Section A";
+                highestIndex = i;
+            }
+
+            if (sectionB[i] > highestQuantity) {
+                highestQuantity = sectionB[i];
+                highestSection = "Section B";
+                highestIndex = i;
+            }
+        }
+
+        System.out.println("Section A Total: " + totalA);
+        System.out.println("Section B Total: " + totalB);
+        System.out.println("Status: " + status);
+        System.out.println("Highest Quantity: " + highestQuantity
+                + " (" + highestSection
+                + ", Item " + (highestIndex + 1) + ")");
+    }
+
+    public static void main(String[] args) {
+
+        int[] sectionA = {20, 15, 30};
+        int[] sectionB = {25, 10, 30};
+
+        analyzeInventory(sectionA, sectionB);
+    }
+}
+
